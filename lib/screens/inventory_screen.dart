cbfcce5c83fab6b7ea5d2df72c3e8caca2c26025
Christian <mohamedchristianpaulsallu@gmail.com>
2025-04-25 10:38:48 +0000

    created a search widget for the inventoryscreen

--- lib/screens/inventory_screen.dart
+++ lib/screens/inventory_screen.dart
@@ -58,6 +58,49 @@ class InventoryScreen extends StatelessWidget {
               ),
             ),
           ),
+          // Search input widget
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.2),
+                    spreadRadius: 1,
+                    blurRadius: 3,
+                    offset: const Offset(0, 1),
+                  ),
+                ],
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search inventory items...',
+                  hintStyle: const TextStyle(color: Colors.grey),
+                  prefixIcon: const Icon(Icons.search, color: Color(0xFF6621DC)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: BorderSide(color: Colors.grey.shade300),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide: const BorderSide(color: Color(0xFF6621DC)),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(vertical: 12.0),
+                  filled: true,
+                  fillColor: Colors.white,
+                ),
+                onChanged: (value) {
+                  // Search functionality will go here
+                },
+              ),
+            ),
+          ),
           Expanded(
             child: Center(
               child: Text(
